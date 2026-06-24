b5b0a5b89c6562348c437b4dd860113414180f58
Ujjwal Darnal <59062@edu.nielsbrock.dk>
2026-06-25 01:45:10 +0200

    Add auto scroll to latest message

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,15 +1,26 @@
 import MessageList from "./MessageList";
 import LoadingIndicator from "./LoadingIndicator";
 import ChatInput from "./ChatInput";
+import { useEffect,useRef } from "react";
 
 function ChatWindow({messages,onAddMessage,isLoading,error}){
+
+    const bottomRef = useRef(null);
+
+ useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages, isLoading]);
+
+
     return(
         <section className="chat-window">
             <MessageList messages = {messages}/>
            {isLoading &&  <LoadingIndicator/>}
 
            {error && <p className="error-message">{error}</p>}
-           
+
+<div ref={bottomRef}></div>
+
             <ChatInput onAddMessage = {onAddMessage}/>
             
         </section>
